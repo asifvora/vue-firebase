a24ee43a312a974f5312ce98f93ff1330957d2a5
asif <asif.vora@multidots.com>
2019-03-28 17:10:31 +0530

    Update .env file and config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,13 @@
 import firebase from 'firebase';
 
-// const config = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.FIREBASE_DB_URL,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-// };
-
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyA_8ns8vBQdpI3qpfXz4JHZX-JI_ICl_wc",
-    authDomain: "vue-firebase-007.firebaseapp.com",
-    databaseURL: "https://vue-firebase-007.firebaseio.com",
-    projectId: "vue-firebase-007",
-    storageBucket: "vue-firebase-007.appspot.com",
-    messagingSenderId: "74691629876"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
